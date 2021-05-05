0bb1209f0caf4e708b5fb0806f2f1f67d7e26885
Leandro Rodrigues <leandrorodrigues.profissional@gmail.com>
2021-05-05 19:11:17 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/Leandro-Rodrigue-s" target="_blank" rel="noreferrer">Leandro Rodrigues Bovenzo</a></p>
+                <p className="text-light">Contato profissional <a href="https://br.linkedin.com/in/leandro-rodrigues-bovenzo-4086b8a2" target="_blank" rel="noreferrer">Linkedin</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
